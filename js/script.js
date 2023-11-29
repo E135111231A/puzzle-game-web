75685aed97497c44ee8b5e43dab06acdeb64cf19
E135111231A <151979520+E135111231A@users.noreply.github.com>
2023-11-30 00:13:30 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,71 +1,88 @@
-class PuzzleGame {
-  constructor(size) {
-    this.size = size;
-    this.board = [];
-    this.init();
-  }
+// Code yang ditempatkan di dalam script.js
+const PuzzleGame = (function() {
+  let size;
+  let board = [];
 
-  init() {
-    for (let i = 0; i < this.size * this.size - 1; i++) {
-      this.board.push(i + 1);
+  class PuzzleGame {
+    constructor(gameSize) {
+      size = gameSize;
+      this.init();
     }
-    this.board.push(null);
-    this.shuffle();
-    this.renderBoard();
-  }
 
-  shuffle() {
-    for (let i = this.board.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.board[i], this.board[j]] = [this.board[j], this.board[i]];
-    }
-  }
-
-  renderBoard() {
-    const puzzleElement = document.getElementById('puzzle');
-    puzzleElement.innerHTML = '';
-    this.board.forEach(num => {
-      const tile = document.createElement('div');
-      tile.className = 'tile';
-      tile.innerText = num ? num : '';
-      tile.addEventListener('click', () => this.move(num));
-      puzzleElement.appendChild(tile);
-    });
-    puzzleElement.style.gridTemplateColumns = `repeat(${this.size}, 100px)`;
-  }
-
-  move(tile) {
-    const emptyIndex = this.board.indexOf(null);
-    const tileIndex = this.board.indexOf(tile);
-
-    if (this.isValidMove(emptyIndex, tileIndex)) {
-      this.board[emptyIndex] = tile;
-      this.board[tileIndex] = null;
+    init() {
+      for (let i = 0; i < size * size - 1; i++) {
+        board.push(i + 1);
+      }
+      board.push(null);
+      this.shuffle();
       this.renderBoard();
-      if (this.isSolved()) {
-        alert('Selamat!! kamu berhasill\nKeberuntunganmu sudah habis :D');
+    }
+
+    shuffle() {
+      for (let i = board.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [board[i], board[j]] = [board[j], board[i]];
       }
-    } else {
-      console.log('Invalid move!');
+    }
+
+    renderBoard() {
+      const puzzleElement = document.getElementById('puzzle');
+      puzzleElement.innerHTML = '';
+      board.forEach(num => {
+        const tile = document.createElement('div');
+        tile.className = 'tile';
+        tile.innerText = num ? num : '';
+        tile.addEventListener('click', () => this.move(num));
+        puzzleElement.appendChild(tile);
+      });
+      puzzleElement.style.gridTemplateColumns = `repeat(${size}, 100px)`;
+    }
+
+    move(tile) {
+      const emptyIndex = board.indexOf(null);
+      const tileIndex = board.indexOf(tile);
+
+      if (this.isValidMove(emptyIndex, tileIndex)) {
+        board[emptyIndex] = tile;
+        board[tileIndex] = null;
+        this.renderBoard();
+        if (this.isSolved()) {
+          alert('Selamat!! kamu berhasil.\nKeberuuntungan mu sudah habis :D');
+        }
+      } else {
+        console.log('Invalid move!');
+      }
+    }
+
+    isValidMove(emptyIndex, tileIndex) {
+      const rowDiff = Math.abs(Math.floor(emptyIndex / size) - Math.floor(tileIndex / size));
+      const colDiff = Math.abs((emptyIndex % size) - (tileIndex % size));
+      const isAdjacent = (rowDiff === 1 && colDiff === 0) || (rowDiff === 0 && colDiff === 1);
+      return isAdjacent;
+    }
+
+    isSolved() {
+      for (let i = 0; i < size * size - 1; i++) {
+        if (board[i] !== i + 1) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    // Getter untuk mendapatkan nilai size
+    static getSize() {
+      return size;
+    }
+
+    // Getter untuk mendapatkan nilai board
+    static getBoard() {
+      return board;
     }
   }
 
-  isValidMove(emptyIndex, tileIndex) {
-    const rowDiff = Math.abs(Math.floor(emptyIndex / this.size) - Math.floor(tileIndex / this.size));
-    const colDiff = Math.abs((emptyIndex % this.size) - (tileIndex % this.size));
-    const isAdjacent = (rowDiff === 1 && colDiff === 0) || (rowDiff === 0 && colDiff === 1);
-    return isAdjacent;
-  }
-
-  isSolved() {
-    for (let i = 0; i < this.size * this.size - 1; i++) {
-      if (this.board[i] !== i + 1) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
+  return PuzzleGame;
+})();
 
 // Inisialisasi game dengan ukuran 3x3 setelah DOM selesai dimuat
 document.addEventListener('DOMContentLoaded', () => {
